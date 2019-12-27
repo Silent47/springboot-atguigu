1b4f30dc4dbbb3d49347fa79ec761c92fbf71393
MengXianBin <mengxianbindev@163.com>
2019-12-27 21:32:12 +0800

    @Value & SpEL

---
+++ springboot-10-yaml-demo/src/main/java/com/mxb/springboot/yamldemo/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.mxb.springboot.yamldemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Love
+ */
+@RestController
+public class HelloController {
+
+    @Value("${test-value}")
+    private String string;
+
+    @Value("#{222 + 234}")
+    private int value;
+
+    @RequestMapping("sayHello")
+    public String sayHello() {
+        return String.format("Hello %s %d!", string, value);
+    }
+
+}
