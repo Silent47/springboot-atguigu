ce5e38ebfdeb0473f2ec3752b0eece909cc0ce18
MengXianBin <mengxianbindev@163.com>
2020-01-08 00:35:13 +0800

    rename package

--- springboot-028-web-rest-crud/src/main/java/com/mxb/WebRestCrudApplication.java
+++ springboot-028-web-rest-crud/src/main/java/com/mxb/WebRestCrudApplication.java
@@ -1,4 +1,4 @@
-package com.mxb.springboot.web.rest.crud;
+package com.mxb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
